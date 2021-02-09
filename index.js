0a010b9a929b212179eed43ab04a0dd005d8cc51
Nick Ver Voort <talkto@nicholasvv.com>
2021-02-09 15:31:10 -0500

    Transform and render JSON into handlebars template with styling

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const fs = require('fs');
-
 const express = require('express');
+const path = require('path');
+const exphbs  = require('express-handlebars');
+
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt();
+
 const app = express();
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname,'/public/')))
+
 const port = 1110;
 
 if (process.argv.length !== 3) {
@@ -9,12 +18,22 @@ if (process.argv.length !== 3) {
     process.exit(1);
 }
 const spellListPath = process.argv[2];
-const spellList = JSON.parse(fs.readFileSync(spellListPath));
+const spellList = processSpellList(JSON.parse(fs.readFileSync(spellListPath)));
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify(spellList));
+  res.render('spells', spellList);
 })
 
 app.listen(port, () => {
   console.log(`spell-sheet-scribe listening on port ${port}`)
 })
+
+function processSpellList(spellListJson) {
+    for (let spellLevel of spellListJson.spellLevels) {
+        spellLevel.saveDc = 10 + spellLevel.level + spellListJson.castingAttributeMod;
+        for (let spell of spellLevel.spells) {
+            spell.description = md.render(spell.description);
+        }
+    }
+    return spellListJson;
+}
